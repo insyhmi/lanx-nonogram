63ffe8669dec5e4c0a779898e1d8376d6f04adea
insyhmi <i.idlan.syahmi@outlook.com>
2025-10-23 16:39:24 +0800

    udpate

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Lanx's Nonogram
 
 My iteration of Nonogram, a game I quite enjoy playing on my spare time
+
+# Objective
+
+The objective of this game is to fill all the squares on the board using clues provided on the sides of the board. 
+
+# Gameplay
+
+The numbers on the side provide information about the number of groups and its amount of crosses on a particular line.
+For instance, the number ```2, 3``` tells you that a group of 2 X's and 3 X's should exist on that line.
+The game concludes when the player successfully fills the board with the correct tiles without dying.
+
+# Controls
+
+Note that the numbers count the crosses (X)
+
+| Control | Action | 
+| :------- | ------: |
+| LMB | Places an X | 
+| RMB | Places a black box|
+
+# Run
+
+To run with the default configuration, simply run the script.
+
+For customs configuration, cd to the directory and run:
+```python main.py [width] [height] [lives]```
+
+
+# Developer notes
+
+During the initialization process, I reckoned that adding some sort of weightage would help setting a fairer game by clumping up X's more frequently.
+The way I implement it is to keep a list of floating numbers [0.15, 0.5, 0.7, 0.91] and select one of them at random for each row to dictate the chances of
+spawning the X on that particular box. 
+Though, I realize it would result in the game generating more frequent 'easy' rows, i.e. a row full of X's or blanks.
+Due to this I changed the weightage implementation to a dynamic range-based, though I haven't tested out its performance.
--- main.py
+++ main.py
@@ -5,8 +5,6 @@ import sys
 
 GAME_PATH = os.path.dirname(__file__)
 ASSET_FILE = os.path.join(GAME_PATH, 'assets')
-WINDOW_HEIGHT = 700
-WINDOW_WIDTH = 700
 WINDOW_TITLE = "Lanx's Nonogram"
 
 WHITE = (255, 255, 255)
@@ -22,46 +20,63 @@ class Nonogram:
         self.ROW_NUMBERS = []
         self.COLUMN_NUMBERS = []
         self.LIVES = lives
+        self.BLACK_BOX = set()
+        self.X_BOX = set()
+        self.CLICKED_BOX = set()
+        self.box_size = 30
         self.allow_click = True
         self.grid_start_x = 50
         self.grid_start_y = 50
-        self.row_num_start_x = 510
+        self.row_num_start_x = self.grid_start_x + 10 + self.height * self.box_size
         self.row_num_start_y = 50
         self.column_num_start_x = self.grid_start_x + 5
-        self.column_num_start_y = 510
-        self.box_size = 30
-    
+        self.column_num_start_y = self.grid_start_y + 10 + self.width * self.box_size
+        self.WINDOW_HEIGHT = max(700, self.height * self.box_size + 300)
+        self.WINDOW_WIDTH = max(700, self.width * self.box_size + 300)
+        self.screen = pygame.display.set_mode((self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
+
+        self.sprite_x = pygame.image.load(os.path.join(ASSET_FILE, 'x.png')).convert_alpha()
+        self.sprite_x = pygame.transform.scale(self.sprite_x, (self.box_size, self.box_size))
         pygame.init()
         pygame.font.init()
         self.FONT = pygame.font.SysFont("Arial", 20)
-        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption(WINDOW_TITLE)
         RUNNING = True
+        
         self.screen_setup()
         self.game_setup()
-
         while RUNNING:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     RUNNING = False
-                elif event.type == pygame.MOUSEBUTTONDOWN: # Mouse click detected
+                elif event.type == pygame.MOUSEBUTTONDOWN and self.allow_click: # Mouse click detected
+                    box_position = self.get_position(event.pos)
                     if event.button == 1: # Left mouse button click
-                        print("Left mouse button")
+                        self.place_x_box(box_position)
                     elif event.button == 3: # Right mouse button click
-                        print("Right mouse button")
+                        self.place_black_box(box_position)
             pygame.display.flip()
+            if self.win():
+                print("You win!")
+                self.game_won()
+            if self.LIVES == 0:
+                self.game_over()
         if not RUNNING:
             self.quit()
-
-
+    
     def game_setup(self):
         # Board initialization
         for i in range(self.height):
             row = []
+            weight = random.random()
             for j in range(self.width):
-                row.append(random.choice(['X', ' ']))
+                entry = random.choices(['X', ' '], weights=[weight, 1 - weight])[0]
+                row.append(entry)
+                if entry == 'X':
+                    self.X_BOX.add((i,j))
+                else:
+                    self.BLACK_BOX.add((i,j))
             self.BOARD.append(row)
-
         # Number Clue initialization
         for row in self.BOARD:
             num = []
@@ -90,21 +105,20 @@ class Nonogram:
                 num.append(i)
             self.COLUMN_NUMBERS.append(num)
         # Placing numbers 
-        for i in range(self.height):
+        for i in range(self.width):
             text = ""
-            for n in self.ROW_NUMBERS[i]:
-                text += str(n) + " "
+            for n in self.COLUMN_NUMBERS[i]:
+                text += str(n) + "  "
             render_text = self.FONT.render(text, True, BLACK)
             self.screen.blit(render_text, (self.row_num_start_x, self.row_num_start_y + self.box_size * i))
         pygame.display.flip()
         
-        for i in range(self.width):
+        for i in range(self.height):
             text = []
             vertical_text_render = []
-            for n in self.COLUMN_NUMBERS[i]:
+            for n in self.ROW_NUMBERS[i]:
                 text.append(str(n))
                 vertical_text_render.append(self.FONT.render(str(n), True, BLACK))
-
             for j, line_surface in enumerate(vertical_text_render):
                 self.screen.blit(line_surface, (self.column_num_start_x + self.box_size * i, self.column_num_start_y + j * 22))
 
@@ -115,6 +129,26 @@ class Nonogram:
                 cell = self.draw_cell((i, j))
                 pygame.draw.rect(self.screen, GRAY, cell)
                 pygame.draw.rect(self.screen, BLACK, cell, 1)
+        self.update_lives(0)
+
+    def win(self):
+        return len(self.CLICKED_BOX) == self.width * self.height
+
+    def game_over(self):
+        self.allow_click = False
+        text_rect = pygame.Rect(self.WINDOW_WIDTH // 2, self.WINDOW_HEIGHT // 2,0, 0)
+        pygame.draw.rect(self.screen, WHITE, text_rect)
+        text_render = pygame.font.SysFont("Arial", 100).render("YOU LOSE", True, (255, 0, 0)) # A big red text only used once.
+        text_surface = text_render.get_rect(center=text_rect.center)
+        self.screen.blit(text_render, text_surface)
+
+    def game_won(self):
+        self.allow_click = False
+        text_rect = pygame.Rect(self.WINDOW_WIDTH // 2, self.WINDOW_HEIGHT // 2,0, 0)
+        pygame.draw.rect(self.screen, WHITE, text_rect)
+        text_render = pygame.font.SysFont("Arial", 100).render("YOU WIN", True, (0, 50, 255))
+        text_surface = text_render.get_rect(center=text_rect.center)
+        self.screen.blit(text_render, text_surface)
 
     def draw_cell(self, pos):
         return pygame.Rect(
@@ -124,5 +158,56 @@ class Nonogram:
             self.box_size
         )
     
+    def update_lives(self, amount):
+        self.LIVES += amount
+        text = f"Lives: {self.LIVES}"
+        text_rect = pygame.Rect(self.WINDOW_WIDTH - 70, self.WINDOW_HEIGHT- 70, 70, 70)
+        pygame.draw.rect(self.screen, WHITE, text_rect)
+        text_render = self.FONT.render(text, True, BLACK)
+        text_surface = text_render.get_rect(center=text_rect.center)
+        self.screen.blit(text_render, text_surface)
+
+    def place_x_box(self, pos):
+        if not pos:
+            return
+        if pos in self.CLICKED_BOX:
+            return
+        x, y = pos
+        if pos in self.X_BOX:
+            self.screen.blit(self.sprite_x, (self.grid_start_x + x * self.box_size, self.grid_start_y + y * self.box_size))
+            pygame.display.flip()
+        else:
+            self.place_black_box(pos)
+            self.update_lives(-1)
+        self.CLICKED_BOX.add(pos)
+    
+    def place_black_box(self, pos):
+        if not pos:
+            return
+        if pos in self.CLICKED_BOX:
+            return
+        x, y = pos
+        if pos in self.BLACK_BOX:
+            clicked_box = self.draw_cell(pos)
+            pygame.draw.rect(self.screen, BLACK, clicked_box)
+            pygame.display.flip()
+        else:
+            self.place_x_box(pos)
+            self.update_lives(-1)
+        self.CLICKED_BOX.add(pos)
+
+    def get_position(self, click_pos):
+        x,y  = click_pos
+        if self.grid_start_x <= x <= self.grid_start_x + self.box_size * self.width and self.grid_start_y <= y <= self.grid_start_y + self.box_size * self.height:
+            x = (x - self.grid_start_x) // self.box_size 
+            y = (y - self.grid_start_y) // self.box_size 
+            return (x, y)
+        return None
+    
 if __name__ == "__main__":
-    Nonogram(15, 15, 3)
+    if len(sys.argv) == 1:
+        Nonogram(15, 15, 3) # The default configuration for a Nonogram game is 15x15 with 3 lives
+    elif len(sys.argv) == 4:
+        Nonogram(int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]))
+    else:
+        print("Invalid arguments")
